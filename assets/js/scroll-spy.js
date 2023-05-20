4c7d97fb7dd9f63f7acd6af2d6d9dc2ca31f9ca0
Ghadeer <ghadeer@futurecode-co.com>
2023-05-20 15:09:50 +0300

    Feat: added animation

--- assets/js/scroll-spy.js
+++ assets/js/scroll-spy.js
@@ -5,20 +5,18 @@ $(document).ready(function () {
 
     $(document).scroll(function () {
         sectionIds.each(function () {
-
+            $('.content').removeClass('fadeUp')
             var container = $(this).attr('href');
             var containerOffset = $(container).offset().top;
             var containerHeight = $(container).outerHeight();
             var containerBottom = containerOffset + containerHeight;
             var scrollPosition = $(document).scrollTop();
-
+            $('.content').addClass('fadeUp')
             if (scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20) {
                 $(this).addClass('active');
             } else {
                 $(this).removeClass('active');
             }
-
-
         });
     });
 
