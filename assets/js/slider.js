99fb0d746a8ab5360d645f713154c31a62dcbf42
Ghadeer <ghadeer@futurecode-co.com>
2023-05-21 12:55:01 +0300

    Feat: added swipe detection to slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,20 @@
 var slideIndex = 0
 
 const slides = document.querySelectorAll('.slide')
+var screenx = 0
+var screeny = 0
+
+document.getElementById('slider').addEventListener('touchstart', (e) => {
+    screenx = e.changedTouches[0].screenX
+    screeny = e.screenY
+})
+document.getElementById('slider').addEventListener('touchend', (e) => {
+    console.log(e.changedTouches[0].screenX)
+    if (screenx != e.changedTouches[0].screenX) {
+        nextSlide()
+        screenx = 0
+    }
+})
 function nextSlide() {
     if (slideIndex === slides.length - 1) slideIndex = 0
     else slideIndex += 1
